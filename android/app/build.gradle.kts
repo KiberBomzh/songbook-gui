cb780bd4b46ab2878a1ca63f9ff2587904e27201
KiberBomzh <klimenokandrej193@gmail.com>
2026-04-06 23:36:58 +0300

    changed package name

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.songbook"
+        applicationId = "com.kiber_bomzh.songbook"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -36,6 +36,10 @@ android {
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
+
+		debug {
+			applicationIdSuffix = ".debug"
+		}
     }
 }
 
