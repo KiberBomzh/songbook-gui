cabaabe02e3e71dc4ee7c181e742e234e18b70d6
KiberBomzh <klimenokandrej193@gmail.com>
2026-04-12 00:11:16 +0300

    added lines wrap in song screen

--- lib/screens/song.dart
+++ lib/screens/song.dart
@@ -20,6 +20,9 @@ final rhythmStyle =
 final textStyle = GoogleFonts.jetBrainsMono();
 
 
+final useLineWrap = true;
+
+
 
 class SongScreen extends StatefulWidget {
 	final String path;
@@ -75,11 +78,16 @@ class SongState extends State<SongScreen> {
 	}
 
 	Widget _buildBody(List<SimpleBlock> blocks) {
+		final screenWidth = MediaQuery.of(context).size.width;
+
 		return SingleChildScrollView(
 			scrollDirection: Axis.horizontal,
 			child: ConstrainedBox(
 				constraints: BoxConstraints(
-					minWidth: MediaQuery.of(context).size.width,
+					minWidth: screenWidth,
+					maxWidth: useLineWrap
+						? screenWidth
+						: double.infinity,
 				),
 				child: SingleChildScrollView(
 					scrollDirection: Axis.vertical,
@@ -123,7 +131,19 @@ class SongState extends State<SongScreen> {
 				...block.lines.map((l) {
 					return switch (l) {
 						SimpleLine_Row(field0: String chords, field1: String rhythm, field2: String text) =>
-							RowWidget(chords: chords, rhythm: rhythm, text: text),
+							RowWidget(
+								chords: chords.isEmpty
+									? null
+									: chords,
+
+								rhythm: rhythm.isEmpty
+									? null
+									: rhythm,
+
+								text: text.isEmpty
+									? null
+									: text
+							),
 						SimpleLine_ChordsLine(field0: String chords) => Text(chords, style: chordsStyle),
 						SimpleLine_PlainText(field0: String text) => Text(text, style: textStyle),
 						SimpleLine_Tab(field0: String tab) => Text(tab, style: textStyle),
@@ -138,9 +158,9 @@ class SongState extends State<SongScreen> {
 }
 
 class RowWidget extends StatelessWidget {
-	final String chords;
-	final String rhythm;
-	final String text;
+	String? chords;
+	String? rhythm;
+	String? text;
 
 	RowWidget({
 		super.key,
@@ -151,17 +171,123 @@ class RowWidget extends StatelessWidget {
 
 	@override
 	Widget build(BuildContext context) {
+
 		return Column(
-			children: [
-				if (chords.isNotEmpty)
-					Text(chords, style: chordsStyle),
-
-				if (rhythm.isNotEmpty)
-					Text(rhythm, style: rhythmStyle),
-
-				if (text.isNotEmpty)
-					Text(text, style: textStyle)
-			],
+			crossAxisAlignment: .start,
+			children: useLineWrap
+				? _getWraped(context)
+				: _getDefault()
 		);
+	}
+
+	List<Widget> _getWraped(BuildContext context) {
+		final textPainter = TextPainter(
+			text: TextSpan(text: 'W', style: textStyle),
+			textDirection: TextDirection.ltr,
+		)..layout();
+		final charWidth = textPainter.width;
+
+		final screenWidth = MediaQuery.of(context).size.width;
+		final maxWidth = screenWidth - 20 - 20;
+
+		final lineLength = _alignLines();
+		final maxLineWidth = charWidth * lineLength;
+
+		if (maxLineWidth < maxWidth)
+			return _getDefault();
+
+
+		final maxChars = (maxWidth / charWidth).floor();
+		List<Widget> lines = [];
+
+		int minusIndent = 0;
+		final linesLength = (lineLength / maxChars).ceil();
+		for (int i = 0; i < linesLength; i++) {
+			final startIndex = (i * maxChars) - minusIndent;
+			int endIndex = (i < linesLength - 1)
+				? (i + 1) * maxChars
+				: lineLength;
+
+
+			if (chords != null && i < linesLength - 1) {
+				while (chords![endIndex] != ' ' && chords![endIndex - 1] != '' && endIndex > startIndex) {
+					minusIndent++;
+					endIndex--;
+				}
+			}
+			if (text != null && i < linesLength - 1) {
+				while (text![endIndex] != ' ' && text![endIndex - 1] != '' && endIndex > startIndex) {
+					minusIndent++;
+					endIndex--;
+				}
+			}
+
+
+			if (chords != null) {
+				lines.add(
+					Text(chords!.substring(startIndex, endIndex), style: chordsStyle)
+				);
+			}
+
+			if (rhythm != null) {
+				lines.add(
+					Text(rhythm!.substring(startIndex, endIndex), style: rhythmStyle)
+				);
+			}
+
+			if (text != null) {
+				lines.add(
+					Text(
+						text!.substring(startIndex, endIndex),
+						style: textStyle
+					)
+				);
+			}
+		}
+
+		return lines;
+	}
+
+	List<Widget> _getDefault() {
+		return [
+			if (chords != null)
+				Text(chords!, style: chordsStyle),
+
+			if (rhythm != null)
+				Text(rhythm!, style: rhythmStyle),
+
+			if (text != null)
+				Text(text!, style: textStyle)
+		];
+	}
+
+	int _getMaxLineLength() {
+		int max = 0;
+		if (chords != null)
+			max = chords!.length;
+
+		if (rhythm != null && rhythm!.length > max)
+			max = rhythm!.length;
+
+		if (text != null && text!.length > max)
+			max = text!.length;
+
+
+		return max;
+	}
+
+	int _alignLines() {
+		int max = _getMaxLineLength();
+
+		if (chords != null && chords!.length < max)
+			chords = chords! +  ' ' * (max - chords!.length);
+
+		if (rhythm != null && rhythm!.length < max)
+			rhythm = rhythm! + ' ' * (max - rhythm!.length);
+
+		if (text != null && text!.length < max)
+			text = text! + ' ' * (max - text!.length);
+
+		return max;
 	}
 }
