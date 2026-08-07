0181d9d39fca178f88dc5cd61516fe7fbedebb5f
KiberBomzh <klimenokandrej193@gmail.com>
2026-08-07 03:08:39 +0300

    settings keys move to their own file

--- lib/services/settings.dart
+++ lib/services/settings.dart
@@ -8,56 +8,11 @@ import 'package:restart_app/restart_app.dart';
 
 import 'package:songbook/main.dart';
 import 'package:songbook/services/preferences.dart';
+import 'package:songbook/services/keys.dart';
 import 'package:songbook/src/rust/api/library.dart' as rust_lib;
 import 'package:songbook/src/rust/api/theory.dart' as rust_theory;
 import 'package:songbook/l10n/app_localizations.dart';
 
-
-// keys
-const String TITLE_COLOR = 'title_color';
-const String TITLE_FONT_FAMILY = 'title_font_family';
-const String IS_TITLE_BOLD = 'is_title_bold';
-const String IS_TITLE_ITALIC = 'is_title_italic';
-
-const String NOTES_COLOR = 'notes_color';
-const String NOTES_FONT_FAMILY = 'notes_font_family';
-const String IS_NOTES_BOLD = 'is_notes_bold';
-const String IS_NOTES_ITALIC = 'is_notes_italic';
-
-const String FINGERINGS_COLOR = 'fingerings_color';
-const String FINGERINGS_FONT_FAMILY = 'fingerings_font_family';
-const String IS_FINGERINGS_BOLD = 'is_fingerings_bold';
-const String IS_FINGERINGS_ITALIC = 'is_fingerings_italic';
-
-const String TAB_COLOR = 'tab_color';
-const String TAB_FONT_FAMILY = 'tab_font_family';
-const String IS_TAB_BOLD = 'is_tab_bold';
-const String IS_TAB_ITALIC = 'is_tab_italic';
-
-const String PLAIN_TEXT_COLOR = 'plain_text_color';
-const String PLAIN_TEXT_FONT_FAMILY = 'plain_text_font_family';
-const String IS_PLAIN_TEXT_BOLD = 'is_plain_text_bold';
-const String IS_PLAIN_TEXT_ITALIC = 'is_plain_text_italic';
-
-
-const String BACKGROUND_COLOR = 'background_color';
-const String TEXT_COLOR = 'text_color';
-const String CHORDS_COLOR = 'chords_color';
-const String RHYTHM_COLOR = 'rhythm_color';
-
-const String IS_DARK_THEME = 'is_dark_theme';
-const String IS_AMOLED = 'is_amoled';
-const String COLOR_ACCENT = 'color_accent';
-const String EDITOR_FONT_SIZE = 'editor_font_size';
-const String EDITOR_FONT_FAMILY = 'editor_font_family';
-const String SONG_FONT_SIZE = 'song_font_size';
-const String SONG_FONT_FAMILY = 'song_font_family';
-const String SHARP_ONLY = 'sharp_only';
-const String LINE_WRAP_IN_SONG = 'line_wrap_in_song';
-const String FINGERING_SIZE_IN_SONG = 'fingering_size_in_song';
-const String BACKGROUND_OPACITY = 'background_opacity';
-const String LANGUAGE = 'language';
-const String ANDROID_CUSTOM_LIB_PATH = 'android_custom_lib_path';
 
 const Map<String, String> LANGUAGES = {
 	'en': 'English',
