85aa1c1c69bce14c2f7aa14ad8134231b30dff0c
KiberBomzh <klimenokandrej193@gmail.com>
2026-08-23 18:49:51 +0300

    added plugin permission_handler

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,11 +6,14 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <permission_handler_windows/permission_handler_windows_plugin.h>
 #include <restart_app/restart_app_plugin_c_api.h>
 #include <share_plus/share_plus_windows_plugin_c_api.h>
 #include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  PermissionHandlerWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
   RestartAppPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("RestartAppPluginCApi"));
   SharePlusWindowsPluginCApiRegisterWithRegistrar(
