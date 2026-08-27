c73c4342bc2b6195b9029cf6a22148d166a788e6
KiberBomzh <klimenokandrej193@gmail.com>
2026-08-28 01:17:57 +0300

    added option save as to song viewer

---
+++ lib/utils/path_picker.dart
@@ -0,0 +1,187 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
+
+import 'package:songbook/functions/get_path_name.dart';
+import 'package:songbook/services/settings.dart';
+
+
+class PathPicker extends StatefulWidget {
+	final String dir;
+
+	const PathPicker({
+		super.key,
+		required this.dir,
+	});
+
+	@override
+	State<PathPicker> createState() => _PathPickerState();
+}
+
+class _PathPickerState extends State<PathPicker> {
+	late SettingsProvider _settings;
+
+	bool _isLoading = false;
+	late final Directory _dir;
+
+	final _dirs = [];
+	final _files = [];
+
+	@override
+	void initState() {
+		super.initState();
+		_dir = Directory(widget.dir);
+		_loadDir();
+	}
+
+	Future<void> _loadDir() async {
+		setState(() => _isLoading = true );
+
+		_dirs.clear();
+		_files.clear();
+
+		await for (final entry in _dir.list(recursive: false, followLinks: true)) {
+			if (entry is Directory) {
+				_dirs.add(entry.path);
+			} else if (entry is File) {
+				_files.add(entry.path);
+			}
+		}
+
+		_dirs.sort();
+		_files.sort();
+
+		setState(() => _isLoading = false );
+	}
+
+
+	@override
+	Widget build(BuildContext context) {
+		_settings = context.watch<SettingsProvider>();
+
+		return Container(
+			decoration: BoxDecoration(
+				image: (_settings.backgroundImage != null)
+					? DecorationImage(
+						image: FileImage(_settings.backgroundImage!),
+						fit: .cover,
+					)
+					: null,
+			),
+			child: Scaffold(
+				appBar: AppBar(
+					title: Text(getPathName(widget.dir)),
+				),
+				body: _buildBody(),
+			),
+		);
+	}
+
+	Widget _buildBody() {
+		if (_isLoading)
+			return Center(
+				child: CircularProgressIndicator(),
+			);
+
+
+		return RefreshIndicator(
+			onRefresh: _loadDir,
+			child: Column(
+				children: [
+					Expanded(
+						child: ListView(
+							children: [
+								..._dirs.map((dir) {
+									return _buildItem(
+										name: getPathName(dir),
+										isDir: true,
+										onTap: () async {
+											final result = await Navigator.push(context,
+												MaterialPageRoute(
+													builder: (context) => PathPicker(dir: dir),
+												),
+											);
+
+											if (result != null && mounted)
+												Navigator.pop(context, result);
+										},
+									);
+								}),
+
+								..._files.map((file) {
+									return _buildItem(
+										name: getPathName(file),
+										isDir: false,
+										onTap: null,
+									);
+								}),
+							],
+						),
+					),
+					
+					const SizedBox(height: 5),
+
+					ElevatedButton(
+						style: ElevatedButton.styleFrom(
+							backgroundColor: Theme.of(context).colorScheme.surfaceContainerHighest,
+						),
+						onPressed: () => Navigator.pop(context, _dir.path),
+						child: SizedBox(
+							width: double.infinity,
+							height: 80,
+							child: Center(
+								child: Text('Select'),
+							),
+						),
+					),
+
+
+					SizedBox(height: MediaQuery.of(context).padding.bottom),
+				],
+			),
+		);
+	}
+
+	Widget _buildItem({
+		required String name,
+		required bool isDir,
+		required VoidCallback? onTap,
+	}) {
+		return Container(
+			margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+			height: 70,
+			decoration: BoxDecoration(
+				borderRadius: .circular(10),
+			),
+			child: Material(
+				color: Theme.of(context).colorScheme.surfaceContainer,
+				clipBehavior: Clip.antiAlias,
+				shape: RoundedRectangleBorder(
+					borderRadius: .circular(10),
+				),
+				child: InkWell(
+					onTap: onTap,
+					child: Container(
+						padding: const .symmetric(horizontal: 15, vertical: 15),
+						child: Row(
+							mainAxisAlignment: .spaceBetween,
+							children: [
+								Icon( (isDir) ? Icons.folder : Icons.music_note),
+								const SizedBox(width: 5),
+
+								Expanded(
+									child: Text(name,
+										maxLines: 2,
+										overflow: TextOverflow.ellipsis,
+									),
+								),
+								const SizedBox(width: 10),
+							],
+						),
+					),
+				),
+			),
+		);
+	}
+}
--- lib/utils/song_viewer.dart
+++ lib/utils/song_viewer.dart
@@ -7,6 +7,7 @@ import 'package:songbook/src/rust/api/library.dart' as lib;
 import 'package:songbook/src/rust/api/song.dart' as song;
 
 import 'package:songbook/screens/song/song.dart';
+import 'package:songbook/utils/path_picker.dart';
 
 
 class SongViewer extends StatelessWidget {
@@ -21,14 +22,55 @@ class SongViewer extends StatelessWidget {
 	Widget build(BuildContext context) {
 		return Scaffold(
 			appBar: AppBar(
-				title: const Text('Song Viewer'),
 				actions: [
 					TextButton(
 						child: Text('Cancel'),
 						onPressed: () => Restart.restartApp(),
 					),
+
+					Spacer(),
+
+					ElevatedButton(
+						child: Text('Save as...'),
+						style: ElevatedButton.styleFrom(
+							backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
+							foregroundColor: Theme.of(context).colorScheme.onSecondaryContainer,
+						),
+						onPressed: () async {
+							try {
+								final s = song.SimpleSong.open(pathStr: path);
+								final libPath = lib.getLibraryPath();
+								final dirPath = await Navigator.push(context,
+									MaterialPageRoute(
+										builder: (context) => PathPicker(dir: libPath),
+									),
+								);
+								if (dirPath == null)
+									return;
+
+								lib.importSong(song: s, dirPath: dirPath);
+								Restart.restartApp();
+							} catch (e) {
+								debugPrint(e.toString());
+								if (context.mounted)
+									ScaffoldMessenger.of(context).showSnackBar(
+										SnackBar(
+											content: Text(AppLocalizations.of(context)!.settingsErrorMsg),
+											duration: Duration(seconds: 3),
+										),
+									);
+							}
+						},
+					),
+
+					const SizedBox(width: 5),
+
 					ElevatedButton(
 						child: Text('Save'),
+						style: ElevatedButton.styleFrom(
+							backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+							foregroundColor: Theme.of(context).colorScheme.onPrimaryContainer,
+						),
 						onPressed: () {
 							try {
 								final s = song.SimpleSong.open(pathStr: path);
